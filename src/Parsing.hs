a15d63ab3d0a90b02e2becd8b7cc1a7ed47794e6
jakosimov <jakobalwall@gmail.com>
2021-07-24 17:25:04 +0200

    Add more basic functionality to parser

--- src/Parsing.hs
+++ src/Parsing.hs
@@ -5,7 +5,8 @@ import Text.Parsec
 import Text.Parsec.String (Parser)
 import Text.Parsec.Combinator
 import Data.Char
-import Data.List (intersperse)
+import System.IO
+import Data.List (intercalate)
 
 newtype CType = CType String
 
@@ -15,8 +16,12 @@ data Expression =
     FunctionCall Identifier [Expression]
   | IdentifierExpression Identifier
   | AssignmentExpression VariableAssignment
-  | StandardIntegerExpression Integer
-  | StandardFloatExpression Integer
+  | StandardIntegerExpression Integer -- Add for other types of int
+  | StandardFloatExpression Integer -- Add for other types of float
+  | AdditionExpression Expression Expression
+  | MultiplicationExpression Expression Expression
+  | SubtractionExpression Expression Expression
+  | DivisionExpression Expression Expression
 
 data VariableDeclaration = VariableDeclaration CType Identifier (Maybe Expression)
 
@@ -24,7 +29,7 @@ data VariableAssignment = VariableAssignment Identifier Expression
 
 data ForInitializer =
     DeclarationInitializer VariableDeclaration
-  | VariableInitializer VariableAssignment
+  | ExpressionInitializer Expression
   | EmptyInitializer
 
 data Statement =
@@ -35,7 +40,7 @@ data Statement =
   | ElseIfStatement Expression Body
   | ElseStatement Body
   | WhileStatement Expression Body
-  | ForStatement ForInitializer Expression Expression Body -- Not correct first argument
+  | ForStatement ForInitializer Expression Expression Body
   | BreakStatement
 
 type Body = [Statement]
@@ -49,6 +54,8 @@ data FunctionDefinition =
                      , functionBody :: Body
                      }
 
+data SourceFile = SourceFile FunctionDefinition
+
 instance Show CType where
   show (CType string) = map toUpper string
 
@@ -61,11 +68,15 @@ instance Show Parameter where
 
 instance Show Expression where
   show expr = case expr of
-    IdentifierExpression i -> show i
-    AssignmentExpression a -> show a
-    StandardIntegerExpression i -> show i
-    StandardFloatExpression f -> show f
-    FunctionCall i ps -> "FUNCTIONCALL"
+    IdentifierExpression i        -> show i
+    AssignmentExpression a        -> show a
+    StandardIntegerExpression i   -> show i
+    StandardFloatExpression f     -> show f
+    FunctionCall i ps             -> "FUNCTIONCALL"
+    AdditionExpression e e'       -> show e ++ " + " ++ show e'
+    SubtractionExpression e e'    -> show e ++ " - " ++ show e'
+    MultiplicationExpression e e' -> show e ++ " * " ++ show e'
+    DivisionExpression e e'       -> show e ++ " / " ++ show e'
 
 instance Show VariableDeclaration where
   show (VariableDeclaration t i expr) =
@@ -78,24 +89,47 @@ instance Show VariableAssignment where
   show (VariableAssignment i expr) =
    show i ++ " := " ++ show expr
 
+showBody :: Body -> String
+showBody body = intercalate "\n" (map show body)
+
+instance Show ForInitializer where
+  show i = case i of
+    DeclarationInitializer v -> show v
+    ExpressionInitializer e  -> show e
+    EmptyInitializer         -> "--"
+
 instance Show Statement where
   show s = case s of
-    DeclarationStatement d -> "_DCLR_ " ++ show d ++ ";;"
-    ExpressionStatement e -> "_EXPR_ " ++ show e ++ ";;"
-    _ -> "???????? :-/"
+    DeclarationStatement d    -> "_DCLR_ " ++ show d ++ ";;"
+    ExpressionStatement e     -> "_EXPR_ " ++ show e ++ ";;"
+    ReturnStatement (Just e)  -> "RETURN " ++ show e ++ ";;"
+    ReturnStatement Nothing   -> "RETURN ;;"
+    IfStatement e b           -> "IF (" ++ show e ++ ")\n" ++ showBody b ++ "\nEND IF"
+    ElseIfStatement e b       -> "ELSEIF (" ++ show e ++ ")\n" ++ showBody b ++ "\nEND ELSE IF"
+    ElseStatement b           -> "ELSE\n" ++ showBody b ++ "\nEND ELSE"
+    WhileStatement e b        -> "WHILE (" ++ show e ++ ")\n" ++ showBody b ++ "\nEND WHILE"
+    ForStatement init c inc b -> "FOR (" ++ show init ++ ";" ++ show c ++ ";"  ++ show inc ++ ")\n" ++ showBody b ++ "\nEND FOR"
+    BreakStatement            -> "BREAK ;;"
+
 
 instance Show FunctionDefinition where
   show FunctionDefinition { returnType, functionName, parameters, functionBody } =
-    "FUNCTION DECLARATION \n" ++
+    "\nFUNCTION DECLARATION \n" ++
     "Return type: " ++ show returnType ++ "\n" ++
     "Name: " ++ show functionName ++ "\n" ++
-    "Parameters: " ++ concat (intersperse ", " (map show parameters)) ++ "\n" ++
-    "Body: \n" ++ concat (intersperse "\n" (map show functionBody))
+    "Parameters: " ++ intercalate ", " (map show parameters) ++ "\n" ++
+    "Body: \n" ++ showBody functionBody
+
+instance Show SourceFile where
+  show (SourceFile d) = show d
 
 testParse :: Parser a -> String -> Either ParseError a
 testParse parser = parse parser "test"
 
 testText = "void hello(int a, char b) { int a = 2; int b; b = 3; }"
+
+spaced :: Parser a -> Parser a
+spaced = between spaces spaces
 
 indentifierCharacter :: Parser Char
 indentifierCharacter = letter <|> digit
@@ -116,6 +150,11 @@ expression :: Parser Expression
 expression =
   do spaces
      result <- choice $ map try [ assignmentExpression
+                                , additionExpression
+                                , subtractionExpression
+                                , multiplicationExpression
+                                , divisionExpression
+                                , parenthesisExpression
                                 , functionCall
                                 , standardIntegerExpression
                                 , standardFloatExpression
@@ -124,20 +163,24 @@ expression =
      spaces
      return result
 
+commaSep :: Parser a -> Parser [a]
+commaSep p = sepBy p (char ',')
+
+inParens :: Parser a -> Parser a
+inParens = between (char '(') (char ')')
+
 functionCall :: Parser Expression
 functionCall =
   do name <- identifier
      spaces
-     char '('
-     arguments <- sepBy expression (char ',')
-     char ')'
+     arguments <- inParens $ commaSep expression
      return $ FunctionCall name arguments
 
 identifierExpression :: Parser Expression
-identifierExpression = fmap IdentifierExpression identifier
+identifierExpression = IdentifierExpression <$> identifier
 
 assignmentExpression :: Parser Expression
-assignmentExpression = fmap AssignmentExpression variableAssignment
+assignmentExpression = AssignmentExpression <$> variableAssignment
 
 standardIntegerExpression :: Parser Expression
 standardIntegerExpression =
@@ -164,6 +207,29 @@ standardFloatExpression =
              Nothing  -> 1
      return $ StandardFloatExpression result
 
+parenthesisExpression :: Parser Expression
+parenthesisExpression = inParens expression
+
+operatorExpression :: Char -> (Expression -> Expression -> Expression) -> Parser Expression
+operatorExpression operator constr  =
+  do a <- expression
+     spaces
+     char operator
+     spaces
+     constr a <$> expression
+
+additionExpression :: Parser Expression
+additionExpression = operatorExpression '+' AdditionExpression
+
+multiplicationExpression :: Parser Expression
+multiplicationExpression = operatorExpression '-' SubtractionExpression
+
+subtractionExpression :: Parser Expression
+subtractionExpression = operatorExpression '*' MultiplicationExpression
+
+divisionExpression :: Parser Expression
+divisionExpression = operatorExpression '/' DivisionExpression
+
 assignmentRightHand :: Parser Expression
 assignmentRightHand =
   do spaces
@@ -186,31 +252,39 @@ variableAssignment :: Parser VariableAssignment
 variableAssignment =
   do spaces
      name <- identifier
-     value <- assignmentRightHand
-     return $ VariableAssignment name value
+     VariableAssignment name <$> assignmentRightHand
 
 statement :: Parser Statement
 statement =
   do spaces
      result <- choice $ map try [ declarationStatement
                                 , expressionStatement
-                                -- , keywordStatement
+                                , returnStatement
+                                , ifStatement
+                                , elseIfStatement
+                                , elseStatement
+                                , whileStatement
+                                , forStatement
+                                , breakStatement
                                 ]
      spaces
      return result
+
+semicolon :: Parser Char
+semicolon = char ';'
 
 declarationStatement :: Parser Statement
 declarationStatement =
   do result <- variableDeclaration
      spaces
-     char ';'
+     semicolon
      return $ DeclarationStatement result
 
 expressionStatement :: Parser Statement
 expressionStatement =
   do result <- expression
      spaces
-     char ';'
+     semicolon
      return $ ExpressionStatement result
 
 returnStatement :: Parser Statement
@@ -219,17 +293,16 @@ returnStatement =
      spaces
      result <- optionMaybe expression
      spaces
-     char ';'
+     semicolon
      return $ ReturnStatement result
+
+condition :: Parser Expression
+condition = between spaces spaces $ inParens expression
 
 ifStatement :: Parser Statement
 ifStatement =
   do string "if"
-     spaces
-     char '('
-     expr <- expression
-     char ')'
-     spaces
+     expr <- condition
      statements <- body
      spaces
      return $ IfStatement expr statements
@@ -238,11 +311,7 @@ ifStatement =
 elseIfStatement :: Parser Statement
 elseIfStatement =
   do string "else if"
-     spaces
-     char '('
-     expr <- expression
-     char ')'
-     spaces
+     expr <- condition
      statements <- body
      spaces
      return $ ElseIfStatement expr statements
@@ -258,17 +327,50 @@ elseStatement =
 whileStatement :: Parser Statement
 whileStatement =
   do string "while"
-     spaces
-     char '('
-     expr <- expression
-     char ')'
-     spaces
+     expr <- condition
      statements <- body
      spaces
      return $ WhileStatement expr statements
 
+forInitializer :: Parser ForInitializer
+forInitializer =
+  choice $ map try [ declarationInitializer
+                   , expressionInitializer
+                   , emptyInitializer
+                   ]
+  where declarationInitializer = DeclarationInitializer <$> variableDeclaration
+        expressionInitializer = ExpressionInitializer <$> expression
+        emptyInitializer      = EmptyInitializer <$ spaces
+
+assocIndex :: Eq a => [[(a, b)]] -> a -> Maybe Int
+assocIndex [] _ = Nothing
+assocIndex (xs:xss) x'
+  | any ((==x') . fst) xs = Just 0
+  | otherwise             = case assocIndex xss x' of
+      Just n  -> Just $ n + 1
+      Nothing -> Nothing
+
+-- data Atom = AtomicIdentifier String |
+
+leftAssociativeOperators :: [[(Char, Expression -> Expression -> Expression)]] -> Parser Expression
+leftAssociativeOperators operations = undefined
+  where atomicExpression = identifier
+        operators        = map fst $ concat operations
+
+
 forStatement :: Parser Statement
-forStatement = undefined
+forStatement =
+  do string "for"
+     spaces
+     char '('
+     init <- spaced forInitializer
+     semicolon
+     condition <- spaced expression
+     semicolon
+     incrementation <- spaced expression
+     char ')'
+     statements <- spaced body
+     return $ ForStatement init condition incrementation statements
 
 breakStatement :: Parser Statement
 breakStatement = fmap (const BreakStatement) (between spaces spaces $ string "break;")
@@ -284,19 +386,10 @@ functionParameter =
 
 functionParameters :: Parser [Parameter]
 functionParameters =
-  do char '('
-     parameters <- sepBy functionParameter (char ',')
-     char ')'
-     return parameters
+  do inParens $ commaSep functionParameter
 
 body :: Parser Body
-body =
-  do char '{'
-     spaces
-     result <- many statement
-     spaces
-     char '}'
-     return result
+body = between (char '{') (char '}') $ spaced $ many statement
 
 functionDefinition :: Parser FunctionDefinition
 functionDefinition =
@@ -312,3 +405,30 @@ functionDefinition =
                                , parameters
                                , functionBody
                                }
+
+sourceFile :: Parser SourceFile
+sourceFile = fmap SourceFile (between spaces spaces functionDefinition)
+
+testFile :: String
+testFile = "test.c"
+
+doPreprocessing :: String -> String
+doPreprocessing string =
+  unlines $ map (removeComments . removePreprocessors) ls
+  where ls = lines string
+        removePreprocessors = takeWhile (/='#')
+        removeComments = third . foldl helper (False, ' ', [])
+        third (_, _, x) = x
+        helper (True, _, xs) c       = (True, c, xs)
+        helper (False, '/', xs) '/'  = (True, '/', xs)
+        helper (False, _, xs) x      = (False, x, xs ++ [x])
+
+testOn :: IO ()
+testOn =
+  do handle <- openFile testFile ReadMode
+     contents <- hGetContents handle
+     let preprocessed = doPreprocessing contents
+     putStrLn preprocessed
+     putStrLn "\nResult: \n"
+     print $ parse sourceFile testFile preprocessed
+     hClose handle
